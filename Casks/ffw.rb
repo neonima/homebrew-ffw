8061538dabef4c9ee0e32066e2e8571a3f53d408
Vincent Gschwend <vincent@crypta.li>
2021-04-19 22:58:29 +0200

    Add caskfile

---
+++ Casks/ffw.rb
@@ -0,0 +1,11 @@
+cask "ffw" do
+  version "64"
+  sha256 "1ad09786be60476fd35827a3e2f52ca721713f7466046408810b8931f2efc3a7"
+
+  url "https://github.com/neonima/ffw/releases/download/v0.1.2/ffw_0.1.2_darwin_amd#{version}.tar.gz"
+  name "ffw"
+  desc "Dead simple tool to rename you file for smooth web access!"
+  homepage "https://github.com/neonima/ffw"
+
+  app "ffw"
+end
